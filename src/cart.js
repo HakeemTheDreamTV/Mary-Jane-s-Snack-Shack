f9cdd2eb6148b438ee3c64376a48141843338431
HakeemTheDreamTV <hakeemthedreamtv@gmail.com>
2024-10-15 13:44:32 -0400

    update

--- src/cart.js
+++ src/cart.js
@@ -19,23 +19,10 @@ let generateCartItems = () => {
         let { id, item } = x;
         let search = shopItemsData.find((y) => y.id === id) || [];
         return `
-        <div class="cart-item">
-        <img width="100" src="${search.img}" alt=""/>
-        <div class="details">
-            <div class="title-price-x">
-              <h4>
-                <p>${search.name}</p>
-                <p>${search.price}</p>
-              </h4>
-              <i class="bi bi-x-lg"></i>
-            </div>
-            <div class="cart-buttons">
-            
-            </div>
-            <h3></h3>
-        </div>
-        </div>
-        `;
+      <div class="cart-item">
+        <img src=${search.img} alt =""/>
+      </div>
+      `;
       })
       .join(""));
   } else {
@@ -43,8 +30,9 @@ let generateCartItems = () => {
     label.innerHTML = `
     <h2>Cart is Empty</h2>
     <a href="index.html">
-        <button class="HomeBtn">Back to Home</button>
+      <button class="HomeBtn">Back to Homepage</button>
     </a>
     `;
   }
 };
+generateCartItems();
--- src/data.js
+++ src/data.js
@@ -1,26 +1,5 @@
 let shopItemsData = [
   {
-    id: "kjsadfoinj",
-    name: "Blackened Chicken Wrap",
-    price: "$9",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    img: "../Photos/Food/Chicken Sandwich.jpg",
-  },
-  {
-    id: "oanjdsfi",
-    name: "Blackened Fish Sandwich",
-    price: "$10",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    img: "../Photos/Food/Fish Sandwich.jpg",
-  },
-  {
-    id: "pwqiaege",
-    name: "Ghost Burger",
-    price: "$12",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    img: "../Photos/Food/Ghost Burger.jpg",
-  },
-  {
     id: "oaiwnrgpoinmwe",
     name: "Mary Jane's Loaded Burger",
     price: "$15",
@@ -33,5 +12,26 @@ let shopItemsData = [
     price: "$9",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     img: "../Photos/Food/Veggie Burger.jpg",
+  },
+  {
+    id: "oanjdsfi",
+    name: "Blackened Fish Sandwich",
+    price: "$10",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    img: "../Photos/Food/Fish Sandwich.jpg",
+  },
+  {
+    id: "kjsadfoinj",
+    name: "Blackened Chicken Wrap",
+    price: "$9",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    img: "../Photos/Food/Chicken Sandwich.jpg",
+  },
+  {
+    id: "pwqiaege",
+    name: "Ghost Burger",
+    price: "$12",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    img: "../Photos/Food/Ghost Burger.jpg",
   },
 ];
