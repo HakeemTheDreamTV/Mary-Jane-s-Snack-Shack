1cc017297bf32cbd0087b34b14f6d1629d770b12
HakeemTheDreamTV <hakeemthedreamtv@gmail.com>
2024-10-15 14:05:06 -0400

    update

--- src/cart.js
+++ src/cart.js
@@ -15,12 +15,29 @@ let generateCartItems = () => {
   if (basket.length !== 0) {
     return (ShoppingCart.innerHTML = basket
       .map((x) => {
-        console.log(x);
         let { id, item } = x;
         let search = shopItemsData.find((y) => y.id === id) || [];
         return `
       <div class="cart-item">
-        <img src=${search.img} alt =""/>
+        <img width="100" src=${search.img} alt="" />
+        <div class="details">
+
+          <div class="title-price-x">
+              <h4 class="title-price">
+                <p>${search.name}</p>
+                <p class="cart-item-price">$ ${search.price}</p>
+              </h4>
+              <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
+          </div>
+
+          <div class="buttons">
+              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+              <div id=${id} class="quantity">${item}</div>
+              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+          </div>
+
+          <h3>$ ${item * search.price}</h3>
+        </div>
       </div>
       `;
       })
@@ -30,7 +47,7 @@ let generateCartItems = () => {
     label.innerHTML = `
     <h2>Cart is Empty</h2>
     <a href="index.html">
-      <button class="HomeBtn">Back to Homepage</button>
+      <button class="HomeBtn">Back to home</button>
     </a>
     `;
   }
